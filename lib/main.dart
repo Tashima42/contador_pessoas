7b453a02eb3e97589c2c1844a64f7442f9fbeb8d
Pedro Tashima <23709916+Tashima42@users.noreply.github.com>
2020-06-22 21:23:11 -0300

    base do layout criada

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,57 @@
 import 'package:flutter/material.dart';
-void main(){
-  
-  runApp(MaterialApp(
-    title: "Contador de Pessoas",
-  ));
 
+void main() {
+  runApp(MaterialApp(
+      title: "Contador de Pessoas",
+      home: Stack(
+        children: <Widget>
+              [
+          Image.asset(
+            'assets/images/space.jpg',
+            fit: BoxFit.cover,
+            height: 1000.0,
+            ),
+          Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas: 0",
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: FlatButton(
+                  child: Text(
+                    "+1",
+                    style: TextStyle(fontSize: 40.0, color: Colors.white),
+                  ),
+                  onPressed: () {
+                    print('+1');
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: FlatButton(
+                  child: Text(
+                    "-1",
+                    style: TextStyle(fontSize: 40.0, color: Colors.white),
+                  ),
+                  onPressed: () {
+                    print('-1');
+                  },
+                ),
+              ),
+            ]),
+            Text(
+              "Pode Entrar!",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0),
+            ),
+          ],
+        ),],
+      )));
 }
